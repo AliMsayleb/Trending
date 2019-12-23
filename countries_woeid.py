8f4909cdfe9956cf25ddc2b936b65302e58cc96c
alimsayleb <alimmsayleb@gmail.com>
2019-12-23 13:13:08 +0200

    Add countries woeid

---
+++ countries_woeid.py
@@ -0,0 +1,317 @@
+countries_woeid = {
+  'WW': {
+    "name": "Worldwide",
+    "country": "Worldwide",
+    "woeid": 1
+  },
+  'AE': {
+    "name": "United Arab Emirates",
+    "country": "United Arab Emirates",
+    "woeid": 23424738
+  },
+  'DZ': {
+    "name": "Algeria",
+    "country": "Algeria",
+    "woeid": 23424740
+  },
+  'AR': {
+    "name": "Argentina",
+    "country": "Argentina",
+    "woeid": 23424747
+  },
+  'AU': {
+    "name": "Australia",
+    "country": "Australia",
+    "woeid": 23424748
+  },
+  'AT': {
+    "name": "Austria",
+    "country": "Austria",
+    "woeid": 23424750
+  },
+  'BH': {
+    "name": "Bahrain",
+    "country": "Bahrain",
+    "woeid": 23424753
+  },
+  'BE': {
+    "name": "Belgium",
+    "country": "Belgium",
+    "woeid": 23424757
+  },
+  'BY': {
+    "name": "Belarus",
+    "country": "Belarus",
+    "woeid": 23424765
+  },
+  'BR': {
+    "name": "Brazil",
+    "country": "Brazil",
+    "woeid": 23424768
+  },
+  'CA': {
+    "name": "Canada",
+    "country": "Canada",
+    "woeid": 23424775
+  },
+  'CL': {
+    "name": "Chile",
+    "country": "Chile",
+    "woeid": 23424782
+  },
+  'CO': {
+    "name": "Colombia",
+    "country": "Colombia",
+    "woeid": 23424787
+  },
+  'DK': {
+    "name": "Denmark",
+    "country": "Denmark",
+    "woeid": 23424796
+  },
+  'DO': {
+    "name": "Dominican Republic",
+    "country": "Dominican Republic",
+    "woeid": 23424800
+  },
+  'EC': {
+    "name": "Ecuador",
+    "country": "Ecuador",
+    "woeid": 23424801
+  },
+  'EG': {
+    "name": "Egypt",
+    "country": "Egypt",
+    "woeid": 23424802
+  },
+  'IE': {
+    "name": "Ireland",
+    "country": "Ireland",
+    "woeid": 23424803
+  },
+  'FR': {
+    "name": "France",
+    "country": "France",
+    "woeid": 23424819
+  },
+  'GH': {
+    "name": "Ghana",
+    "country": "Ghana",
+    "woeid": 23424824
+  },
+  'DE': {
+    "name": "Germany",
+    "country": "Germany",
+    "woeid": 23424829
+  },
+  'GR': {
+    "name": "Greece",
+    "country": "Greece",
+    "woeid": 23424833
+  },
+  'GT': {
+    "name": "Guatemala",
+    "country": "Guatemala",
+    "woeid": 23424834
+  },
+  'ID': {
+    "name": "Indonesia",
+    "country": "Indonesia",
+    "woeid": 23424846
+  },
+  'IN': {
+    "name": "India",
+    "country": "India",
+    "woeid": 23424848
+  },
+  'IT': {
+    "name": "Italy",
+    "country": "Italy",
+    "woeid": 23424853
+  },
+  'JP': {
+    "name": "Japan",
+    "country": "Japan",
+    "woeid": 23424856
+  },
+  'JO': {
+    "name": "Jordan",
+    "country": "Jordan",
+    "woeid": 23424860
+  },
+  'KE': {
+    "name": "Kenya",
+    "country": "Kenya",
+    "woeid": 23424863
+  },
+  'KR': {
+    "name": "Korea",
+    "country": "Korea",
+    "woeid": 23424868
+  },
+  'KW': {
+    "name": "Kuwait",
+    "country": "Kuwait",
+    "woeid": 23424870
+  },
+  'LB': {
+    "name": "Lebanon",
+    "country": "Lebanon",
+    "woeid": 23424873
+  },
+  'LV': {
+    "name": "Latvia",
+    "country": "Latvia",
+    "woeid": 23424874
+  },
+  'OM': {
+    "name": "Oman",
+    "country": "Oman",
+    "woeid": 23424898
+  },
+  'MX': {
+    "name": "Mexico",
+    "country": "Mexico",
+    "woeid": 23424900
+  },
+  'MY': {
+    "name": "Malaysia",
+    "country": "Malaysia",
+    "woeid": 23424901
+  },
+  'NG': {
+    "name": "Nigeria",
+    "country": "Nigeria",
+    "woeid": 23424908
+  },
+  'NL': {
+    "name": "Netherlands",
+    "country": "Netherlands",
+    "woeid": 23424909
+  },
+  'NO': {
+    "name": "Norway",
+    "country": "Norway",
+    "woeid": 23424910
+  },
+  'NZ': {
+    "name": "New Zealand",
+    "country": "New Zealand",
+    "woeid": 23424916
+  },
+  'PE': {
+    "name": "Peru",
+    "country": "Peru",
+    "woeid": 23424919
+  },
+  'PK': {
+    "name": "Pakistan",
+    "country": "Pakistan",
+    "woeid": 23424922
+  },
+  'PL': {
+    "name": "Poland",
+    "country": "Poland",
+    "woeid": 23424923
+  },
+  'PA': {
+    "name": "Panama",
+    "country": "Panama",
+    "woeid": 23424924
+  },
+  'PT': {
+    "name": "Portugal",
+    "country": "Portugal",
+    "woeid": 23424925
+  },
+  'QA': {
+    "name": "Qatar",
+    "country": "Qatar",
+    "woeid": 23424930
+  },
+  'PH': {
+    "name": "Philippines",
+    "country": "Philippines",
+    "woeid": 23424934
+  },
+  'PR': {
+    "name": "Puerto Rico",
+    "country": "Puerto Rico",
+    "woeid": 23424935
+  },
+  'RU': {
+    "name": "Russia",
+    "country": "Russia",
+    "woeid": 23424936
+  },
+  'SA': {
+    "name": "Saudi Arabia",
+    "country": "Saudi Arabia",
+    "woeid": 23424938
+  },
+  'ZA': {
+    "name": "South Africa",
+    "country": "South Africa",
+    "woeid": 23424942
+  },
+  'SG': {
+    "name": "Singapore",
+    "country": "Singapore",
+    "woeid": 23424948
+  },
+  'ES': {
+    "name": "Spain",
+    "country": "Spain",
+    "woeid": 23424950
+  },
+  'SE': {
+    "name": "Sweden",
+    "country": "Sweden",
+    "woeid": 23424954
+  },
+  'CH': {
+    "name": "Switzerland",
+    "country": "Switzerland",
+    "woeid": 23424957
+  },
+  'SY': {
+    "name": "Syria",
+    "country": "Syria",
+    "woeid": 23424956
+  },
+  'TH': {
+    "name": "Thailand",
+    "country": "Thailand",
+    "woeid": 23424960
+  },
+  'TR': {
+    "name": "Turkey",
+    "country": "Turkey",
+    "woeid": 23424969
+  },
+  'GB': {
+    "name": "United Kingdom",
+    "country": "United Kingdom",
+    "woeid": 23424975
+  },
+  'UA': {
+    "name": "Ukraine",
+    "country": "Ukraine",
+    "woeid": 23424976
+  },
+  'US': {
+    "name": "United States",
+    "country": "United States",
+    "woeid": 23424977
+  },
+  'VE': {
+    "name": "Venezuela",
+    "country": "Venezuela",
+    "woeid": 23424982
+  },
+  'VN': {
+    "name": "Vietnam",
+    "country": "Vietnam",
+    "woeid": 23424984
+  }
+}
--- country_fetcher.py
+++ country_fetcher.py
@@ -1,11 +1,27 @@
 import requests
 
 
-def get_country(ip_address):
+def get_country_code(ip_address):
     try:
         response = requests.get("http://ip-api.com/json/{}".format(ip_address))
         js = response.json()
-        country = js['country']
+        country = js['countryCode']
         return country
     except Exception as e:
-        return "Unknown"
+        return "WW"
+
+
+def get_country_id(country_code):
+    import countries_woeid
+    if country_code in countries_woeid.countries_woeid:
+        return countries_woeid.countries_woeid[country_code]['woeid']
+    else:
+        return 1
+
+
+def get_country_name(country_code):
+    import countries_woeid
+    if country_code in countries_woeid.countries_woeid:
+        return countries_woeid.countries_woeid[country_code]['country']
+    else:
+        return "WorldWide"
