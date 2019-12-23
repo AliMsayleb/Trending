1588e4dd2667841e8fce4155a5253feff3b1938d
alimsayleb <alimmsayleb@gmail.com>
2019-12-23 13:12:30 +0200

    Add twitter services and configurations

--- twitter_fetcher.py
+++ twitter_fetcher.py
@@ -1,5 +1,35 @@
-def get_trending_hashtags(country):
-    return [{
-        'name': 'كلن_يعني_سعد',
-        'link': 'https://twitter.com/search?q=%23%D9%83%D9%84%D9%86_%D9%8A%D8%B9%D9%86%D9%8A_%D8%B3%D8%B9%D8%AF&src=trend_click'
-    }]
+import requests
+from bearer_token import twitter_bearer_token
+from config import OAuthEncodedToken
+
+
+def get_trending_tags(woeid):
+    response = send_request(twitter_bearer_token, woeid)
+    if 'errors' in response and 'Invalid or expired token.' == response['errors'][0]['message']:
+        token = get_new_token()
+        response = send_request(token, woeid)
+    if 'errors' in response and 'Sorry, that page does not exist.' == response['errors'][0]['message']:
+        return []
+    trends = response[0]['trends']
+
+    result = []
+    for tag in trends:
+        result.append({'name': tag['name'], 'link': tag['url']})
+
+    return result
+
+
+def get_new_token():
+    headers = {'Authorization': 'Basic ' + OAuthEncodedToken, 'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'}
+    response = requests.post('https://api.twitter.com/oauth2/token', data='grant_type=client_credentials', headers=headers).json()
+    token = response['access_token']
+    file = open('bearer_token.py', 'w')
+    file.write("twitter_bearer_token = '" + token + "'")
+
+    return token
+
+
+def send_request(token, woeid):
+    headers = {'Authorization': 'Bearer ' + token}
+
+    return requests.get("https://api.twitter.com/1.1/trends/place.json?id=" + str(woeid), headers=headers).json()
